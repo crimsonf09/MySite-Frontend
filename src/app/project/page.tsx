abee57063bc8c4b446a5eb8829c47fe4b92d2fdc
Natouch Srikiatden <natouchf40@gmail.com>
2025-07-15 01:18:27 +0700

    feat:search2

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -3,9 +3,21 @@
 import SearchBar from "@/components/SearchBar";
 import { useState } from "react";
 
+export interface projectStructure {
+  "title": string,
+  "shortDescription": string,
+  "description": string,
+  "techStack": [string],
+  "url": string,
+  "type": [string],
+  "images": [string],
+  "shortMedia": string
+
+}
 
 export default function Project(){
     const [search,setSearch] = useState('')
+    const projects:[projectStructure] = fetch('url+/projects',)
     return (
         <main className="min-h-screen bg-[#0a0a0a] text-white flex flex-col justify-center px-4 py-16 mt-20 gap-4">
             <div className='text-2xl'>Project</div>
