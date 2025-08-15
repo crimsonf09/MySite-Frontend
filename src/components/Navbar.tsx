7c68f6b9d558a02e3896ed163547b435eaf897f7
Natouch Srikiatden <natouchf40@gmail.com>
2025-08-15 16:31:31 +0700

    fix: Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import "../app/globals.css"
 export default function Navbar() {
   const navItems = [
     { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
+    { label: "About Me", href: "/aboutme" },
     { label: "Project", href: "/project" },
     { label: "Contact", href: "/contact" },
   ];
